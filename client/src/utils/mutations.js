28f28b03b45852e94a218aff871baa3195fbcd4c
dmadon <deanna.madon@gmail.com>
2022-10-20 23:13:28 -0500

    define queries and mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,47 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+    mutation login($email:String!, $password:String!){
+        login(email:$email, password:$password){
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation addUser($username:String!, $email:String!, $password:String!){
+        addUser(username:$username, email:$email, password:$password){
+            token
+            user{
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($input: SaveBookInput!){
+        saveBook(input:$input){
+            user{
+                _id
+                username
+                email
+                bookCount
+                savedBooks{
+                    bookId
+                    authors
+                    description
+                    link
+                    title
+                    image
+                }
+
+            }
+        }
+    }
+`;
