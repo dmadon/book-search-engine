b46ec1db4176e941d7352e03bd94e435f404ebdf
dmadon <deanna.madon@gmail.com>
2022-10-21 20:27:43 -0500

    replace saveBook API route with saveBook graphql mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -40,11 +40,11 @@ const resolvers = {
 
             return {token,user};
         },
-        saveBook: async (parent, args, context)  => {
+        saveBook: async (parent, {input}, context)  => {
             if(context.user) {
                 const updatedUser = await User.findOneAndUpdate(
                     {_id:context.user._id},
-                    {$addToSet:{ savedBooks: args}},
+                    {$addToSet:{ savedBooks: input}},
                     {new: true, runValidators: true}
                 );
                 return updatedUser;
